c071141f5bae91ec77b26f02afbb2d93f70a30e3
Amit Yadav <yadavamit006@gmail.com>
2025-01-29 16:07:55 +0530

    Java changes - DSA changes

---
+++ src/main/java/com/learning/ay/dsa/array/LongestSubArrayWithSumKPositive.java
@@ -0,0 +1,47 @@
+package com.learning.ay.dsa.array;
+
+public class LongestSubArrayWithSumKPositive {
+    public static void main(String[] args) {
+        bruteForceApproach();
+        bruteForceApproach2();
+    }
+
+    private static void bruteForceApproach() {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int n = a.length;
+
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                long s = 0;
+                for (int K = i; K <= j; K++) {
+                    s += a[K];
+                }
+                if (s == k) {
+                    len = Math.max(len, j - i + 1);
+                }
+            }
+        }
+        System.out.println("(bruteForceApproach) LongestSugArraySum of length " + len);
+    }
+
+    //Better space complexity O(n2)
+    private static void bruteForceApproach2() {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int n = a.length;
+
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            long s = 0;
+            for (int j = i; j < n; j++) {
+                s += a[j];
+                if (s == k) {
+                    len = Math.max(len, j - i + 1);
+                }
+            }
+        }
+        System.out.println("(bruteForceApproach) LongestSugArraySum of length " + len);
+    }
+}
